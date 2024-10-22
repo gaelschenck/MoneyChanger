7ff524c469902d3d1ab689d5e75a5312ea0b5a99
michelineuh <gaelschenck@hotmail.fr>
2024-10-22 16:23:39 +0200

    v4

--- Moneychanger_TU/public/Convert.php
+++ Moneychanger_TU/public/Convert.php
@@ -1,5 +1,5 @@
 <?php
-// Remplacer par votre clé API ExchangeRate-API
+
 $apiKey = 'b20ba3dd0044e79aae25b1ff';
 
 // Récupération des données du formulaire
@@ -9,35 +9,34 @@ $amount = $_POST['amount'];
 
 // URL de l'API pour obtenir le taux de change
 $apiUrl = "https://v6.exchangerate-api.com/v6/$apiKey/pair/$fromCurrency/$toCurrency";
+$response_json = file_get_contents($apiUrl);
 
-// Initialiser une session cURL
-$curl = curl_init($apiUrl);
+// Continuing if we got a result
+if(false !== $response_json) {
 
-// Définir les options cURL
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    // Try/catch for json_decode operation
+    try {
 
-// Exécuter la requête cURL et obtenir la réponse
-$response = curl_exec($curl);
+		// Decoding
+		$response = json_decode($response_json);
 
-// Fermer la session cURL
-curl_close($curl);
+		// Check for success
+		if('success' === $response->result) {
+            //Taux de change
+            $exchangeRate = $response->conversion_rate;
 
-// Décoder la réponse JSON
-$exchangeData = json_decode($response, true);
+            //Calcul de la conversion
+            $convertedAmount = $amount * $exchangeRate;
 
-// Vérifier si la requête a réussi
-if ($exchangeData && $exchangeData['result'] === 'success') {
-    // Taux de change
-    $exchangeRate = $exchangeData['conversion_rate'];
-    
-    // Calcul de la conversion
-    $convertedAmount = $amount * $exchangeRate;
-
-    echo "<h1>Résultat de la conversion</h1>";
-    echo "<p>$amount $fromCurrency équivaut à $convertedAmount $toCurrency</p>";
-    echo "<p>Taux de change: 1 $fromCurrency = $exchangeRate $toCurrency</p>";
-} else {
-    // En cas d'erreur
-    echo "<p>Erreur lors de la récupération des taux de change. Veuillez vérifier vos devises ou réessayer plus tard.</p>";
+            echo "<h1>Résultat de la conversion</h1>";
+            echo "<p>$amount $fromCurrency équivaut à $convertedAmount $toCurrency</p>";
+            echo "<p>Taux de change: 1 $fromCurrency = $exchangeRate $toCurrency</p>";
+        }
+    }
+    catch(Exception $e) {
+        echo "ZOB";
+        echo "<p>Erreur lors de la récupération des taux de change. Veuillez vérifier vos devises ou réessayer plus tard.</p>";
+        echo "<pre>Réponse brute de l'API : " . print_r($response_json, true) . "</pre>";
+    }
 }
 ?>
